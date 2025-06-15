1fbc628fcd49c59755d94d6313012dcf3a390464
mermajax <mermajax@gmail.com>
2025-06-16 01:14:03 +0200

    Initial commit: Liquidity Manager

---
+++ src/LiquidityManager.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "./interfaces/IV2Router02.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract LiquidityManager {
+    using SafeERC20 for IERC20;
+    address public V2Router02;
+
+    event Swap(address tokenIn, address tokenOut, uint amountIn, uint amountOut);
+    event AddLiquidity(address tokenA, address tokenB, uint lpAmount);
+
+    constructor(address _V2router02) {
+        V2Router02 = _V2router02;
+    }
+
+    function swapTokens(uint amountIn, uint amountOutMin, address[] memory path, uint deadline) public returns (uint) {
+        IERC20(path[0]).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(path[0]).approve(V2Router02, amountIn);
+        uint[] memory amounts = IV2Router02(V2Router02).swapExactTokensForTokens(amountIn, amountOutMin, path, msg.sender, deadline);
+
+        emit Swap(path[0], path[path.length - 1], amountIn, amounts[amounts.length - 1]);
+
+        return amounts[amounts.length - 1];
+    }
+
+    function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, uint deadline) public returns (uint) {
+        IERC20(tokenA).safeTransferFrom(msg.sender, address(this), amountADesired);
+        IERC20(tokenB).safeTransferFrom(msg.sender, address(this), amountBDesired);
+
+        IERC20(tokenA).approve(V2Router02, amountADesired);
+        IERC20(tokenB).approve(V2Router02, amountBDesired);
+
+        (, , uint liquidity) = IV2Router02(V2Router02).addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin, msg.sender, deadline);
+        emit AddLiquidity(tokenA, tokenB, liquidity);
+        return liquidity;
+    }
+
+    function addLiquiditySingleToken(address[] memory path, uint amountIn, uint amountOutMin, uint amountAMin, uint amountBMin, uint deadline) public {
+        IERC20(path[1]).safeTransferFrom(msg.sender, address(this), amountIn / 2);
+        uint amountOut = swapTokens(amountIn / 2, amountOutMin, path, deadline);
+
+        IERC20(path[0]).approve(V2Router02, amountIn / 2);
+        IERC20(path[1]).approve(V2Router02, amountOut);
+
+        (uint amountA, uint amountB, uint liquidity) = IV2Router02(V2Router02).addLiquidity(
+            path[0],
+            path[1],
+            amountIn / 2,
+            amountOut,
+            amountAMin,
+            amountBMin,
+            msg.sender,
+            deadline
+        );
+        if (amountIn / 2 > amountA) {
+            IERC20(path[0]).safeTransfer(msg.sender, amountIn / 2 - amountA);
+        }
+
+        if (amountOut > amountB) {
+            IERC20(path[1]).safeTransfer(msg.sender, amountOut - amountB);
+        }
+
+        emit AddLiquidity(path[0], path[1], liquidity);
+    }
+}
---
+++ test/LiquidityManager.t.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import "../src/LiquidityManager.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract LiquidityManagerTest is Test {
+    LiquidityManager lm;
+    address uniswapV2Router02 = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+    address USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+    address USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address user = 0xF977814e90dA44bFA03b6295A0616a897441aceC;
+
+    function setUp() public {
+        lm = new LiquidityManager(uniswapV2Router02);
+    }
+
+    function testSwap() public {
+        vm.startPrank(user);
+        uint amountIn = 100 * 1e6;
+        uint amountOutMin = 98 * 1e6;
+        uint deadline = block.timestamp + 2 minutes;
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = USDC;
+
+        uint aBalanceBefore = IERC20(USDT).balanceOf(user);
+        uint bBalanceBefore = IERC20(USDC).balanceOf(user);
+
+        IERC20(USDT).approve(address(lm), amountIn);
+        uint amountOut = lm.swapTokens(amountIn, amountOutMin, path, deadline);
+
+        uint aBalanceAfter = IERC20(USDT).balanceOf(user);
+        uint bBalanceAfter = IERC20(USDC).balanceOf(user);
+
+        assertEq(aBalanceBefore, aBalanceAfter + amountIn);
+        assertEq(bBalanceBefore + amountOut, bBalanceAfter);
+
+        vm.stopPrank();
+    }
+
+    function testAddLiquidity() public {
+        vm.startPrank(user);
+
+        uint amountADesired = 111 * 1e6;
+        uint amountBDesired = 111 * 1e6;
+        uint deadline = block.timestamp + 2 minutes;
+
+        uint aBalanceBefore = IERC20(USDT).balanceOf(user);
+        uint bBalanceBefore = IERC20(USDC).balanceOf(user);
+
+        IERC20(USDT).approve(address(lm), amountADesired);
+        IERC20(USDC).approve(address(lm), amountBDesired);
+        lm.addLiquidity(USDT, USDC, amountADesired, amountBDesired, (amountADesired * 98) / 100, (amountBDesired * 98) / 100, deadline);
+
+        uint aBalanceAfter = IERC20(USDT).balanceOf(user);
+        uint bBalanceAfter = IERC20(USDC).balanceOf(user);
+
+        assertTrue(aBalanceAfter < aBalanceBefore);
+        assertTrue(bBalanceAfter < bBalanceBefore);
+
+        vm.stopPrank();
+    }
+}
