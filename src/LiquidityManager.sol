c36951222a34defd88a16960219bd6330cbbbed2
0xteric <101284609+0xteric@users.noreply.github.com>
2025-06-26 23:37:01 +0200

    Documentation: Natspec

--- src/LiquidityManager.sol
+++ src/LiquidityManager.sol
@@ -20,6 +20,13 @@ contract LiquidityManager {
         V2Factory = _V2Factory;
     }
 
+    /**
+     * Swaps any amount of tokens available on Uniswap V2 router pools
+     * @param amountIn amount of the token in
+     * @param amountOutMin minimum amount to receive of the token out
+     * @param path array of token addresses [tokenIn,tokenOut]
+     * @param deadline deadline time for the swap to complete
+     */
     function swapTokens(uint amountIn, uint amountOutMin, address[] memory path, uint deadline) public returns (uint) {
         IERC20(path[0]).safeTransferFrom(msg.sender, address(this), amountIn);
         IERC20(path[0]).approve(V2Router02, amountIn);
@@ -30,6 +37,16 @@ contract LiquidityManager {
         return amounts[amounts.length - 1];
     }
 
+    /**
+     * Adds liquidity to a V2 Uniswap pool
+     * @param tokenA token A address
+     * @param tokenB token B address
+     * @param amountADesired amount of token A to add
+     * @param amountBDesired amount of token B to add
+     * @param amountAMin minimum amount of token A to add
+     * @param amountBMin minimum amount of token B to add
+     * @param deadline deadline for the transaction to be completed
+     */
     function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, uint deadline) public returns (uint) {
         IERC20(tokenA).safeTransferFrom(msg.sender, address(this), amountADesired);
         IERC20(tokenB).safeTransferFrom(msg.sender, address(this), amountBDesired);
@@ -41,6 +58,16 @@ contract LiquidityManager {
         emit AddLiquidity(tokenA, tokenB, liquidity);
         return liquidity;
     }
+
+    /**
+     * Adds liquidity to a V2 Uniswap pool receiving only one of the tokens of the pool
+     * @param path token address array [tokenA, tokenB]
+     * @param amountIn amount of token A sent
+     * @param amountOutMin minimum amount of tokenB to receive
+     * @param amountAMin minimum amount of token A to add in the pool
+     * @param amountBMin minimum amount of token B to add in the pool
+     * @param deadline deadline for the transaction to be completed
+     */
 
     function addLiquiditySingleToken(address[] memory path, uint amountIn, uint amountOutMin, uint amountAMin, uint amountBMin, uint deadline) public returns (uint) {
         IERC20(path[0]).safeTransferFrom(msg.sender, address(this), amountIn);
@@ -59,6 +86,15 @@ contract LiquidityManager {
         return liquidity;
     }
 
+    /**
+     * Removes the liquidity from the pool
+     * @param tokenA token A address
+     * @param tokenB token B address
+     * @param liquidity amount of lp tokens
+     * @param amountAMin minimum amount of token A to receive
+     * @param amountBMin minimum amount of token B to receive
+     * @param deadline deadline for the transaction to be completed
+     */
     function removeLiquidity(address tokenA, address tokenB, uint liquidity, uint amountAMin, uint amountBMin, uint deadline) external {
         address lp = IV2Factory(V2Factory).getPair(tokenA, tokenB);
         IERC20(lp).safeTransferFrom(msg.sender, address(this), liquidity);
